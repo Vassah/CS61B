28a8690f1315d492cbb9c79e77b609459a580849
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-13 16:48:39 -0700

    Update HW2.scala

--- HW2.scala
+++ HW2.scala
@@ -1,3 +1,5 @@
+import java.io.*;
+
 class Date {
   val months_to_days = Map(1 -> 31, 2 -> 28, 3 -> 31, 4 -> 30, 5 -> 31, 6 -> 30, 7 -> 31, 8 -> 31, 9 -> 30, 10 -> 31, 11 -> 30, 12 -> 31)
   val months_to_numb = Map("January" -> 1, "February" -> 2, "March" -> 3, "April" -> 4,"May" -> 5,"June" -> 6,"July" -> 7,"August" -> 8,"September" -> 9,"October" -> 10,"November" -> 11,"December" -> 12)
